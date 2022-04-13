bc0a48f294201803e20848c1a48d297f5ae9276b
LastJonicorn <joni.k.lampela@gmail.com>
2022-04-13 10:45:02 +0300

    Kuvat näkyy

--- index.sql
+++ index.sql
@@ -67,34 +67,34 @@ INSERT INTO tuoteryhma(tuoteryhmanimi)
     ("Miniatyyrit")
 ;
 
-INSERT INTO tuote(tuotenimi,hinta,tietoa,tuoteryhmanro)
+INSERT INTO tuote(tuotenimi,hinta,kuva,tietoa,tuoteryhmanro)
     VALUES 
-    ("Carcasonne","45","Peli, jossa pelivälineenänne toimii itse pelilauta.",1),
-    ("Kimble","20","Tutustu vanhaan klassikkoon uudestaan.",1),
-    ("Monopoly","20","Vain yksi voi olla Wallstreetin kunkku. Oletko se sinä?",1),
+    ("Carcasonne","45","Shakki.png","Peli, jossa pelivälineenänne toimii itse pelilauta.",1),
+    ("Kimble","20","Shakki.png","Tutustu vanhaan klassikkoon uudestaan.",1),
+    ("Monopoly","20","Shakki.png","Vain yksi voi olla Wallstreetin kunkku. Oletko se sinä?",1),
 
-    ("Dominion","35","Deck building",2),
-    ("Cards against humanity","45","Korttipeli kamalille ihmisille.",2),
-    ("Skip-Bo","15","Kuka saa pinonsa tyhjäksi ensin?",2),
+    ("Dominion","35","Shakki.png","Deck building",2),
+    ("Cards against humanity","45","Shakki.png","Korttipeli kamalille ihmisille.",2),
+    ("Skip-Bo","15","Shakki.png","Kuka saa pinonsa tyhjäksi ensin?",2),
 
-    ("Dead Of Winter","65","Strategiapelien ystävälle.",3),
-    ("Shakki","30","Klassisten strategiapelien ystävälle.",3),
-    ("Risk","40","Maailma kaipaa valloittajaa.",3),
+    ("Dead Of Winter","65","Shakki.png","Strategiapelien ystävälle.",3),
+    ("Shakki","30","Shakki.png","Klassisten strategiapelien ystävälle.",3),
+    ("Risk","40","Shakki.png","Maailma kaipaa valloittajaa.",3),
 
-    ("Alias","25","Täydellinen peli koko perheen peli-iltaan.",4),
-    ("Twister","20","Riittääkö tasapainosi ja venyvyytesi haasteeseen?",4),
-    ("UNO","15","Klassista hasukaa koko perheen kesken",4),
+    ("Alias","25","Shakki.png","Täydellinen peli koko perheen peli-iltaan.",4),
+    ("Twister","20","Shakki.png","Riittääkö tasapainosi ja venyvyytesi haasteeseen?",4),
+    ("UNO","15","Shakki.png","Klassista hasukaa koko perheen kesken",4),
 
-    ("Dungeons and Dragons starter pack","25","Hyvä paikka aloittaa matka D&D:n mielenkiintoiseen maailmaan",5),
-    ("Scythe","49","Roolipelien ystävälle",5),
-    ("Hero Quest","119","Roolipelien ystävälle",5),
+    ("Dungeons and Dragons starter pack","25","Shakki.png","Hyvä paikka aloittaa matka D&D:n mielenkiintoiseen maailmaan",5),
+    ("Scythe","49","Shakki.png","Roolipelien ystävälle",5),
+    ("Hero Quest","119","Shakki.png","Roolipelien ystävälle",5),
 
-    ("Yatzy","20","Heitä noppaa ja toivo parasta!",6),
-    ("Kimble","20","Tutustu vanhaan klassikkoon uudestaan.",6),
-    ("Monopoly","20","Vain yksi voi olla Wallstreetin kunkku. Oletko se sinä?",6),
+    ("Yatzy","20","Shakki.png","Heitä noppaa ja toivo parasta!",6),
+    ("Kimble","20","Shakki.png","Tutustu vanhaan klassikkoon uudestaan.",6),
+    ("Monopoly","20","Shakki.png","Vain yksi voi olla Wallstreetin kunkku. Oletko se sinä?",6),
 
-    ("Weleho Figuuri","40","Täydellinen tapa tuoda luonnetta lautapeli-iltaan. Oma figuuri!",7),
-    ("Warrior","20","Täydellinen tapa tuoda luonnetta lautapeli-iltaan. Oma figuuri!",7),
-    ("Mage","20","Täydellinen tapa tuoda luonnetta lautapeli-iltaan. Oma figuuri!",7)
+    ("Weleho Figuuri","40","Shakki.png","Täydellinen tapa tuoda luonnetta lautapeli-iltaan. Oma figuuri!",7),
+    ("Warrior","20","Shakki.png","Täydellinen tapa tuoda luonnetta lautapeli-iltaan. Oma figuuri!",7),
+    ("Mage","20","Shakki.png","Täydellinen tapa tuoda luonnetta lautapeli-iltaan. Oma figuuri!",7)
 
 ;
