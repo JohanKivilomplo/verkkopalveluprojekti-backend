c0cd0094e5b2c9fb24c89b8cd3529c7fbaa0c606
Johankivilompolo <Johankivi@hotmail.com>
2022-04-20 13:21:25 +0300

    loginia eteenpäin

--- inc/login_view.php
+++ inc/login_view.php
@@ -1,3 +1,13 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kirjautuminen</title>
+</head>
+<body> -->
+
 <?php
 require_once 'functions.php';
 require_once 'headers.php';
@@ -19,17 +29,26 @@ if(!isset($_SESSION["username"]) && isset($uname)){
 }
 
     if(!isset($_SESSION["username"])){
-?>
+        
+    ?>
 
 
-    <form action="../src/modules/login.php" method="post">
+
+
+
+        <form action="../src/modules/login.php" method="post">
         <label for="username">Username:</label><br>
         <input type="text" name="username" id="username"><br>
         <label for="password">Password:</label><br>
         <input type="password" name="password" id="password"><br>
         <input type="submit" class="btn btn-primary" value="Log in">
-    </form>
+        </form>
 
+    <?php } ?>
+
+        
+<!-- </body>
+</html> -->
 
 
 
