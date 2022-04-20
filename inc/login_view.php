45d3425a6712ed8338411ce5bdd24b2dd5e2b87e
Johankivilompolo <Johankivi@hotmail.com>
2022-04-20 12:39:34 +0300

    loginia pävitetty

--- inc/login_view.php
+++ inc/login_view.php
@@ -1,18 +1,37 @@
 <?php
 require_once 'functions.php';
 require_once 'headers.php';
+include('../src/login.php');
 
-include('../src/login.php'); /* <-- Osoite ei vielä oikea */
-    echo '<form action="../src/modules/login.php" method="post">
+$uname = filter_input(INPUT_POST, "username");
+$pw = filter_input(INPUT_POST, "password");
+
+if(!isset($_SESSION["username"]) && isset($uname)){
+
+    try {
+        login($uname, $pw);
+        header("Location: index.php");
+        exit;
+    } catch (Exception $e) {
+        echo '<div class="alert alert-danger" role="alert">'.$e->getMessage().'</div>';
+    }
+   
+}
+
+    if(!isset($_SESSION["username"])){
+?>
+
+
+    <form action="../src/modules/login.php" method="post">
         <label for="username">Username:</label><br>
         <input type="text" name="username" id="username"><br>
         <label for="password">Password:</label><br>
         <input type="password" name="password" id="password"><br>
-        <input type="submit" class="btn btn-primary" value "Log in">
-    </form>';
-include('../'); // muuta osoite
+        <input type="submit" class="btn btn-primary" value="Log in">
+    </form>
 
-?>
+
+
 
     
     
