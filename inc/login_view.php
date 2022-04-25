b3f48daa8fe444f9d6215354e1cfb8af061bb11e
LastJonicorn <joni.k.lampela@gmail.com>
2022-04-25 13:47:19 +0300

    toimii taas??

--- inc/login_view.php
+++ inc/login_view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kirjautuminen</title>dsfsdf
+    <title>Kirjautuminen</title>
 </head>
 <body> -->
 
@@ -35,18 +35,20 @@ if(!isset($_SESSION["username"]) && isset($uname)){
 
 
 
-        <form action="../src/login.php" method="post">
+
+        <form action="../src/modules/login.php" method="post">
         <label for="username">Username:</label><br>
         <input type="text" name="username" id="username"><br>
         <label for="password">Password:</label><br>
         <input type="password" name="password" id="password"><br>
         <input type="submit" class="btn btn-primary" value="Log in">
         </form>
-        
-    <?php } include 'footer.php'; ?>
+
+    <?php } ?>
 
         
-
+<!-- </body>
+</html> -->
 
 
 
